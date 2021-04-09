7e83de600a4de26888026c590ec2079948a3d340
York Delloyd <eyok@hotmail.com>
2021-04-08 21:36:40 -0400

    Added a procfile

---
+++ routes/index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+
+/* GET home page. */
+router.get('/teams', function(req, res, next) {
+      axios.get('https://raw.githubusercontent.com/ydelloyd/Datasets/master/teams.json')
+    .then(response => {
+        res.send(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
+
+router.get('/leaderboard', function(req, res, next) {
+  axios.get('https://www.espn.com/golf/leaderboard')
+    .then(response => {
+      if (response) {
+        const $ = cheerio.load(response.data)
+        const $trs = $('.competitors tbody tr')
+        const leaderboard = {};
+        $trs.toArray().forEach(tr => {
+          const tds = $(tr).find('td').toArray();
+          const playerPos = $(tds[0]).text();
+          const playerName = $(tds[1]).text();
+          const playerScore = $(tds[2]).text();
+          leaderboard[playerName.replace(" (a)", "")] = {
+            "pos": playerPos,
+            "toPar": playerScore === "E" ? "0" : playerScore
+          }
+        });
+        res.send(leaderboard);
+      } else {
+        console.log("There was an error");
+      }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+  
+});
+
+module.exports = router;
+
+
+// 
